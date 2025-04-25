c03c51df5bb96792dd56064e71ac8ef5e9b2af62
Avinash Guleria <avinashguleria1009@gmail.com>
2025-04-25 10:02:01 +0530

    done

--- Frontend/src/Routes.js
+++ Frontend/src/Routes.js
@@ -1,4 +1,4 @@
-import LoginPage from "./pages/login.jsx";
+import LoginPage from "./pages/Login.jsx";
 import SignupPage from "./pages/Signup";
 import Home from "./pages/Home";
 import CreateProduct from './pages/createProduct.jsx';
